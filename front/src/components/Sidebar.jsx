220a6de0e2e0f0921bf8afd6610f97737040750e
Harman <singh9765h@gmail.com>
2023-03-16 02:02:22 +0530

    Frontend Navbar implemented

--- front/src/components/Sidebar.jsx
+++ front/src/components/Sidebar.jsx
@@ -9,8 +9,13 @@ import {links} from "../data/dummy"
 
 
 const Sidebar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext()
-  // const activeMenu = true;
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext()
+  
+  const handleCloseSideBar = ()=> {
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normal = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-800 text-md dark:text-gray-200 hover:text-black hover:bg-light-gray m-2'
@@ -22,7 +27,7 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className='flex justify-between items-center'>
-            <Link to="/" onClick={()=> setActiveMenu(false)} 
+            <Link to="/" onClick={handleCloseSideBar} 
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
               <SiShopware />  <span>Shopy</span>
             </Link>
@@ -41,7 +46,7 @@ const Sidebar = () => {
                   {item.title}
                 </p>
                 {item.links.map((link)=> (
-                  <NavLink to={`/${link.name}`} key={link.name} onClick={()=> {}} className={({isActive})=> isActive ? activeLink : normal }>
+                  <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSideBar} className={({isActive})=> isActive ? activeLink : normal }>
                       {link.icon}
                       <span className='capitalize'>{link.name}</span>
                   </NavLink>
